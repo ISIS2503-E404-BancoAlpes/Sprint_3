b7fa484ce1a4569064fec1e8fb59c111d90a2f92
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2024-05-09 20:38:24 -0500

    Update models.py

--- banco-app/bancoAlpes_app/solicitudes/models.py
+++ banco-app/bancoAlpes_app/solicitudes/models.py
@@ -4,8 +4,8 @@ class Solicitud(models.Model):
     id= models.IntegerField(primary_key = True) 
     tipo= models.CharField(max_length=50)
     fecha= models.DateField(auto_created=True)   #Fecha de creación
-    cliente= models.IntegerField(max_length=50)
-    llave= models.CharField(max_length=50)
+    cliente= models.CharField(max_length=50,default='0000000')
+    llave= models.CharField(max_length=64,default='0000000')
     def __str__(self):
         return '%s , %s, %s,%s'%(self.id, self.fecha,self.cliente,self.tipo)
 # Create your models here.
